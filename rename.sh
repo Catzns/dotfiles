2b89d4353ad7d15d6bd7ed8fa9416b4605806163
Catzs <195701838+Catzns@users.noreply.github.com>
2025-06-27 16:58:19 -0600

    Added a script to rename files while maintaining symlinks

---
+++ rename.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+usage() {
+	printf "%s\n" "USAGE: ./$(basename $0) [OLD NAME] [NEW NAME]"
+	exit 1
+}
+
+if [[ $# -ne 2 ]]; then
+	usage
+fi
+
+dotfile_dir=$(dirname $(realpath "$0"))
+name_old="$1"
+name_new="$2"
+
+link=$(grep "^$name_old	" "$dotfile_dir/LINKS" || printf "%s\n" "INVALID RENAME: Specified file is not currently linked." >&2; exit 1)
+link=$(printf "%s" "${link#*	}"
+link=$(printf "%s" "${link/\~/$HOME}")
+
+if find "$dotfile_dir"/"$name_new" &> /dev/null; then
+	printf "%s\n" "INVALID RENAME: Target file already exists." >&2
+	exit 1
+elif ! find "$dotfile_dir"/"$name_old" &> /dev/null; then
+	printf "%s\n" "INVALID RENAME: Specified file does not exist." >&2
+	exit 1
+fi
+
+sed -i "s|^$name_old	|$name_new	|" "$dotfile_dir/LINKS"
+mv -n --no-copy "$name_old" "$name_new"
+ln -sf "$dotfile_dir/$name_new" "$link"
+
+printf "%s\n" "Renamed '$name_old' to '$name_new'."
